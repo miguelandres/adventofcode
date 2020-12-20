8d49109c6c5106f77c27bb446a499ffb44c54cfc
Miguel Barreto <miguelandres@users.noreply.github.com>
2020-12-19 19:52:21 -0500

    Finish d2, add d3

--- src/main/scala/dev/miguely/aoc/d2/PasswordPolicy.scala
+++ src/main/scala/dev/miguely/aoc/d2/PasswordPolicy.scala
@@ -2,30 +2,50 @@ package dev.miguely.aoc.d2
 import scala.io.Source
 import scala.util.matching.Regex
 
-case class Policy(char: String, min: Int, max: Int)
+//Counting Policy is first part
+case class CountingPolicy(char: String, min: Int, max: Int)
+//PositionalPolicy is second part
+case class PositionalPolicy(char: Char, pos1: Int, pos2: Int)
 
 object PasswordPolicy {
   val passwordPattern: Regex = "^([0-9]+)-([0-9]+) ([^\\s]): ([^\\s]+)$".r
 
   def main(args: Array[String]): Unit = {
     var count = 0
-    Source.fromFile("input/d2p1.txt")
-    .getLines()
-    .foreach( line =>{
-      var matchesOption = passwordPattern.findFirstMatchIn(line)
-      val matches = matchesOption.get
-      val policy = Policy(
-        matches.group(3),
-        matches.group(1).toInt,
-        matches.group(2).toInt
-      )
-      if (compliesWithPolicy(matches.group(4), policy)) count = count + 1
-    })
+    Source
+      .fromFile("input/d2.txt")
+      .getLines()
+      .foreach(line => {
+        var matchesOption = passwordPattern.findFirstMatchIn(line)
+        val matches = matchesOption.get
+        // Create CountingPolicy for the first part and PositionalPolicy for the second part
+        // val policy = CountingPolicy(
+        // matches.group(3),
+        // matches.group(1).toInt,
+        // matches.group(2).toInt
+        // )
+        val policy = PositionalPolicy(
+          matches.group(3).charAt(0),
+          matches.group(1).toInt - 1,
+          matches.group(2).toInt - 1
+        )
+        if (compliesWithPolicy(matches.group(4), policy)) 
+          count = count + 1
+      })
     println(count)
   }
 
-  def compliesWithPolicy(password: String, policy: Policy): Boolean = {
+  def compliesWithPolicy(password: String, policy: CountingPolicy): Boolean = {
     val matches = policy.char.r.findAllIn(password).length
     return (matches >= policy.min && matches <= policy.max)
+  }
+
+  def compliesWithPolicy(
+      password: String,
+      policy: PositionalPolicy
+  ): Boolean = {
+    return (password.charAt(policy.pos1) != password.charAt(policy.pos2) &&
+      (password.charAt(policy.pos1) == policy.char || 
+       password.charAt(policy.pos2) == policy.char))
   }
 }
---
+++ src/main/scala/dev/miguely/aoc/d3/Slopes.scala
@@ -0,0 +1,26 @@
+package dev.miguely.aoc.d3
+
+import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
+
+object Slopes extends App {
+  var map = new ArrayBuffer[String]()
+  Source.fromFile("input/d3.txt").getLines().foreach(map += _)
+
+  val directions = Array[(Int, Int)]((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
+
+  var mult: Long = 1
+  for ((dj, di) <- directions) {
+    var j = 0
+    var i = 0
+    var count = 0
+    while (i < map.size) {
+      if (map(i)(j) == '#') count = count + 1
+      j += dj
+      j %= map(i).size
+      i += di
+    }
+    mult = mult * (count)
+  }
+  println (mult)
+}
